f617b263d6acbbc45d9ebb1ce7194c6fff311b2c
Sara Ibraheem <65045743+Sara-Ibraheem@users.noreply.github.com>
2023-02-01 00:52:18 +0500

    Add files via upload

---
+++ fyp_feature_reduction.m
@@ -0,0 +1,7 @@
+clear all
+close all
+clc
+% X = xlsread('five_classes_deep_learning and emd.xlsx');
+% mappedA = compute_mapping(X(:,1:end-1), 'KernelPCA',50);
+% label=X(:,end);
+% data = [mappedA label];
